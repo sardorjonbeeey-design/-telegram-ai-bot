6482732e681d367e46a01fe8b5df1cd394df41e1
sardorjonbeeey-design <sardorjonbeeey@gmail.com>
2026-07-14 20:08:42 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo "=== Installing Python packages ==="
 pip install -r requirements.txt
+
+echo "=== Installing Chromium ==="
 playwright install chromium
+
+echo "=== Build finished ==="
